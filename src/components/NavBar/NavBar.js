20c1441c1e94866da6199439055b9a27acc0c897
Juank628 <jsanchez_zunino@yahoo.com>
2021-02-04 23:16:54 -0500

    navbar dropdown skeleton

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,29 +1,51 @@
-import React from "react";
-import "./NavBar.css"
+import React, { useState } from "react";
+import "./NavBar.css";
 import chicken_icon from "../../img/chicken_icon.svg";
+import menu_icon from "../../img/menu_icon.svg";
+import { navLinksInfo } from "./NavBar.json";
 
 export default function NavBar() {
+  const [fullMenuClassName, setFullMenuClassName] = useState("display-none");
+
+  const showFullMenu = () => {
+    if (fullMenuClassName === "display-none") {
+      setFullMenuClassName("full-page-menu");
+    } else {
+      setFullMenuClassName("display-none");
+    }
+  };
+
   return (
-    <div className="navbar-container">
-      <div>
-        <img src={chicken_icon} alt="logo" className="navbar-logo" />
+    <React.Fragment>
+      <div className="navbar-container">
+        <div className="navbar-main-btn">
+          <img src={chicken_icon} alt="logo" className="navbar-logo" />
+          <img
+            src={menu_icon}
+            alt="button"
+            className="navbar-menu-btn"
+            onClick={showFullMenu}
+          />
+        </div>
+        <div className="navbar-list-container">
+          <ul>
+            {navLinksInfo.map((link, index) => (
+              <li key={index}>
+                <a href={link.ref}>{link.text}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
-      <div className="navbar-list-container">
+      <div className={fullMenuClassName}>
         <ul>
-          <li>
-            <a href="">item</a>
-          </li>
-          <li>
-            <a href="">item</a>
-          </li>
-          <li>
-            <a href="">item</a>
-          </li>
-          <li>
-            <a href="">item</a>
-          </li>
+          {navLinksInfo.map((link, index) => (
+            <li key={index}>
+              <a href={link.ref}>{link.text}</a>
+            </li>
+          ))}
         </ul>
       </div>
-    </div>
+    </React.Fragment>
   );
 }
